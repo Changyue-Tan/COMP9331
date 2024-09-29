7c6f2350d0639c6d895e2794aa4a31f2c0e0b43c
Changyue Tan <z5325184@ad.unsw.edu.au>
2024-09-30 03:23:21 +1000

    ass in progress

--- ass/assign/client.py
+++ ass/assign/client.py
@@ -3,70 +3,79 @@ import time
 import socket
 import sys
 
+def send_heartbeat(client_socket, username):
+    heartbeat_code = "HBT"
+    heartbear_msg = f"{heartbeat_code} {username}"
+    while not stop_heartbeat:
+        client_socket.sendto(heartbear_msg.encode(), server_address)
+        time.sleep(2)
 
-def authenticate(client_socket):
-    while True:
-        username = input("Enter username: ")
-        password = input("Enter password: ")
-        code = "AUTH"
-        payload = f"{code} {username} {password}"
-
-        client_socket.sendto(payload.encode(), ('127.0.0.1', server_port))
-        response, _ = client_socket.recvfrom(1024)
-
-        if response.decode() == "OK":
-            print("Welcome to BitTrickle!")
-            print("Available commands are: get, lap, lpf, pub, sch, unp, xit")
-            return username, client_socket
-        else:
-            print("Authentication failed. Please try again.")
-
-def send_heartbeat():
-    print("Sending heartbeat to server...")
-    time.sleep(2)
-
-def prompt_for_commands():
-    while True:
-        command = input("> ").strip()
-
-        if command.startswith("get "):
-            filename = command.split(" ")[1]
-            print(f"Attempting to get file '{filename}' from active peers...")
-
-        elif command == "lap":
-            print("Listing all active peers...")
-
-        elif command == "lpf":
-            print("Listing all published files by this user...")
-
-        elif command.startswith("pub "):
-            filename = command.split(" ")[1]
-            print(f"Publishing file '{filename}'...")
-
-        elif command.startswith("sch "):
-            substring = command.split(" ")[1]
-            print(f"Searching for files containing '{substring}'...")
-
-        elif command.startswith("unp "):
-            filename = command.split(" ")[1]
-            print(f"Unpublishing file '{filename}'...")
-
-        elif command == "xit":
-            print("Exiting BitTrickle...")
-            break
-
-        else:
-            print("Unknown command. Please try again.")
-
-if __name__ == "__main__":
+server_port = int(sys.argv[1])
+server_IP = '127.0.0.1'
+server_address = (server_IP, server_port)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
-    server_port = int(sys.argv[1])
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+while True:
+    auth_code = "AUTH"
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    client_request = f"{auth_code} {username} {password}"
 
-    username = authenticate(client_socket)
-    send_heartbeat()
-    HB_thread = threading.Thread(target=server.handle_client, args=(username))
-    prompt_for_commands()
+    client_socket.sendto(client_request.encode(), server_address)
+    server_response, _ = client_socket.recvfrom(1024)
+    
+    message = server_response.decode().split(' ')
+    respose_type = message[0]
+    respose_note = message[1]
+
+    if respose_type == "OK":
+        print("Welcome to BitTrickle!")
+        break
+    else:
+        print("Authentication failed. Please try again.")
+
+
+HB_thread = threading.Thread(target=send_heartbeat, args=(client_socket, username))
+stop_heartbeat = False
+print("Starting heartbeat...")
+HB_thread.start()
+
+print("Available commands are: get, lap, lpf, pub, sch, unp, xit")
+while True:
+    command = input("> ").strip()
+
+    if command.startswith("get "):
+        filename = command.split(" ")[1]
+        print(f"Attempting to get file '{filename}' from active peers...")
+        
+    elif command == "lap":
+        print("Listing all active peers...")
+        
+    elif command == "lpf":
+        print("Listing all published files by this user...")
+        
+    elif command.startswith("pub "):
+        filename = command.split(" ")[1]
+        print(f"Publishing file '{filename}'...")
+        
+    elif command.startswith("sch "):
+        substring = command.split(" ")[1]
+        print(f"Searching for files containing '{substring}'...")
+        
+    elif command.startswith("unp "):
+        filename = command.split(" ")[1]
+        print(f"Unpublishing file '{filename}'...")
+        
+    elif command == "xit":
+        print("Exiting BitTrickle...")
+        break
+    else:
+        print("Unknown command. Please try again.")
+
+print("Stoping heartbeat...")
+stop_heartbeat = True
+HB_thread.join()
+
 
     
 
--- ass/assign/server.py
+++ ass/assign/server.py
@@ -1,78 +1,92 @@
 import socket
-import threading
 import sys
 import datetime
+import time
 
-class BitTrickleClient:
-    def __init__(self, username, client_address):
-        self.username = username
-        self.address = client_address  # (IP, port) tuple
-        self.availability = False
-    
-    def __str__(self):
-        return f"Client(username={self.username}, address={self.address})"
+def load_credentials(credentials_file):
+    credentials = {}
+    with open(credentials_file, 'r') as f:
+        for line in f:
+            username, password = line.strip().split(' ')
+            credentials[username] = password
+    return credentials
 
-class BitTrickleServer:
-    def __init__(self, server_port, credentials_file):
+def display_msg_recieved(client_port, request_type, username):
+    current_time = datetime.datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    msg = f"Received {request_type} from {username}"
+    print(f"{current_time}: {client_port}: {msg}")
 
-        self.credentials = {}
-        with open(credentials_file, 'r') as f:
-            for line in f:
-                username, password = line.split(' ')
-                self.credentials[username] = password
-            
-        self.active_clients = {}
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.server_socket.bind(('127.0.0.1', server_port))
+def display_msg_sent(client_port, response_type, username):
+    current_time = datetime.datetime.now().strftime("%H:%M:%S.%f")[:-3]
+    msg = f"Sent {response_type} to {username}"
+    print(f"{current_time}: {client_port}: {msg}")
 
-    def authenticate(self, username, password):
-        if username in self.credentials and self.credentials[username] == password:
-            return True
-        else:
+def check_active(username):
+    last_heartbeat_time = heartbeats_record[username]
+    current_time = time.time()
+    if username in active_clients:
+        if (current_time - last_heartbeat_time) > 3:
+            active_clients.remove(username)
             return False
-
-    def handle_client(self, payload, client_address):
-        
-        message = payload.decode().split(' ')
-        code = message[0]
-
-        if code == 'AUTH':
-            username = message[1]
-            password = message[2]
-            print(f"Received {code} from {username}")
-            self.authenticate(username, password)
         else:
-            pass
+            return True
+    else:
+        False
 
- 
+def check_credentials(username, password):
+    return username in credentials and credentials[username] == password
 
-        if self.authenticate(username, password):
-            client = BitTrickleClient(username, client_address)
-            self.active_clients[client_address] = client
-            response = "OK"
-        else:
-            response = "ERR"
-        
-        current_time = datetime.datetime.now().strftime("%H:%M:%S.%f")[:-3]
-        client_port = client_address[1]
-        self.server_socket.sendto(response.encode(), client_address)
-        print(f"{current_time}: {client_port}: Sent {response} to {username}")
+def handle_AUTH():
+    request_type = "AUTH"
+    client_port = client_address[1]
+    username = client_request[1]
+    password = client_request[2]
+    display_msg_recieved(client_port, request_type, username)
+
+    response_type = "ERR"
+    response_note = "Authentication failed"
+
+    if not check_active(username) and check_credentials(username, password):
+        response_type = "OK"
+        response_note = "Authentication successful"
+        active_clients.add(username)
+        heartbeats_record[username] = time.time() # auth treated as a heartbeat
+
+    response = f"{response_type} {response_note}"
+    display_msg_sent(client_port, response_type, username)
+    server_socket.sendto(response.encode(), client_address)
+    return active_clients
+
+def handle_HBT():
+    request_type = "HBT"
+    client_port = client_address[1]
+    username = client_request[1]
+    display_msg_recieved(client_port, request_type, username)
+    heartbeats_record[username] = time.time()
 
 
+server_port = int(sys.argv[1])
+server_IP = '127.0.0.1'
+server_address = (server_IP, server_port)
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(server_address)
+
+credentials = load_credentials('credentials.txt')
+
+active_clients = set() # a set for unique usernames
+heartbeats_record = {username: 0 for username in credentials}  # last heartbeat time for each user, initialised to be 0
+
+print(f"Server is listening for requests...")
+
+while True:
+    client_request, client_address = server_socket.recvfrom(1024)
+    client_request = client_request.decode().split(' ')
+    request_type = client_request[0]
+    match request_type:
+        case "AUTH":
+            active_clients = handle_AUTH()
+        case "HBT":
+            handle_HBT()
 
 
-
-
-
-if __name__ == "__main__":
-
-    credentials_file = 'credentials.txt'
-    server_port = int(sys.argv[1])
-    server = BitTrickleServer(server_port, credentials_file) 
-
-    print(f"Server is listening for authetication requests...")
-    
-    while True:
-        payload, client_address = server.server_socket.recvfrom(1024)
-        client_thread = threading.Thread(target=server.handle_client, args=(payload, client_address))
-        client_thread.start()
