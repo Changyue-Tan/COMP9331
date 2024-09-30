a518d569a4bad367290593e43fefff4d2572cf8f
z5325184 <z5325184@vx14.orchestra.cse.unsw.EDU.AU>
2024-09-30 15:50:52 +1000

    ass in progress

--- ass/assign/client.py
+++ ass/assign/client.py
@@ -4,17 +4,42 @@ import socket
 import sys
 
 def send_heartbeat(client_socket, username):
+    '''
+    send hearbeat to server at interval of 2 seconds
+    to be used as a thread
+    '''
     heartbeat_code = "HBT"
-    heartbear_msg = f"{heartbeat_code} {username}"
+    heartbeat_msg = f"{heartbeat_code} {username}"
     while not stop_heartbeat:
-        client_socket.sendto(heartbear_msg.encode(), server_address)
+        client_socket.sendto(heartbeat_msg.encode(), server_address)
         time.sleep(2)
 
+def send_and_recieve_lap_request():
+    lap_code = "LAP"
+    lap_msg = f"{lap_code} {username}"
+    print("sending lap request...")
+    client_socket.sendto(lap_msg.encode(), server_address)
+    server_response, _ = client_socket.recvfrom(1024)
+    print("received a lap response")
+    server_response = server_response.decode().split(' ')
+    number_of_active_peers = int(server_response[1])
+    if number_of_active_peers == 0:
+        print("No active peers")
+        return
+    else:
+        print(f"{number_of_active_peers} active peer")
+    active_peers = server_response[2:]
+    for peer in active_peers:
+        print(peer)
+
+
+# create socket
 server_port = int(sys.argv[1])
 server_IP = '127.0.0.1'
 server_address = (server_IP, server_port)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
+
+# prompt for authentication
 while True:
     auth_code = "AUTH"
     username = input("Enter username: ")
@@ -24,9 +49,9 @@ while True:
     client_socket.sendto(client_request.encode(), server_address)
     server_response, _ = client_socket.recvfrom(1024)
     
-    message = server_response.decode().split(' ')
-    respose_type = message[0]
-    respose_note = message[1]
+    server_response = server_response.decode().split(' ')
+    respose_type = server_response[0]
+    respose_note = server_response[1]
 
     if respose_type == "OK":
         print("Welcome to BitTrickle!")
@@ -34,12 +59,13 @@ while True:
     else:
         print("Authentication failed. Please try again.")
 
-
+# start heartbeat thread
 HB_thread = threading.Thread(target=send_heartbeat, args=(client_socket, username))
 stop_heartbeat = False
 print("Starting heartbeat...")
 HB_thread.start()
 
+# prompt for user cmd
 print("Available commands are: get, lap, lpf, pub, sch, unp, xit")
 while True:
     command = input("> ").strip()
@@ -47,34 +73,38 @@ while True:
     if command.startswith("get "):
         filename = command.split(" ")[1]
         print(f"Attempting to get file '{filename}' from active peers...")
-        
+        continue
     elif command == "lap":
         print("Listing all active peers...")
-        
+        send_and_recieve_lap_request()
+        continue
     elif command == "lpf":
         print("Listing all published files by this user...")
-        
+        continue
     elif command.startswith("pub "):
         filename = command.split(" ")[1]
         print(f"Publishing file '{filename}'...")
-        
+        continue
     elif command.startswith("sch "):
         substring = command.split(" ")[1]
         print(f"Searching for files containing '{substring}'...")
-        
+        continue
     elif command.startswith("unp "):
         filename = command.split(" ")[1]
         print(f"Unpublishing file '{filename}'...")
-        
+        continue
     elif command == "xit":
         print("Exiting BitTrickle...")
         break
     else:
         print("Unknown command. Please try again.")
 
+# stop heartbeat thread and close UDP cocket
 print("Stoping heartbeat...")
 stop_heartbeat = True
 HB_thread.join()
+print("Closing socket...")
+client_socket.close()
 
 
     
--- ass/assign/server.py
+++ ass/assign/server.py
@@ -37,6 +37,10 @@ def check_credentials(username, password):
     return username in credentials and credentials[username] == password
 
 def handle_AUTH():
+    '''
+    respond to client with "ERR" if username is already active, or credentials not match
+    respond to client wtih "OK" if if successfuly added client to active_clients set
+    '''
     request_type = "AUTH"
     client_port = client_address[1]
     username = client_request[1]
@@ -55,7 +59,6 @@ def handle_AUTH():
     response = f"{response_type} {response_note}"
     display_msg_sent(client_port, response_type, username)
     server_socket.sendto(response.encode(), client_address)
-    return active_clients
 
 def handle_HBT():
     request_type = "HBT"
@@ -64,29 +67,35 @@ def handle_HBT():
     display_msg_recieved(client_port, request_type, username)
     heartbeats_record[username] = time.time()
 
+def handle_LAP():
+    pass
+
+def handle_LPF():
+    pass
+
 
 server_port = int(sys.argv[1])
 server_IP = '127.0.0.1'
 server_address = (server_IP, server_port)
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(server_address)
 
-credentials = load_credentials('credentials.txt')
-
+credentials = load_credentials('credentials.txt') # credentials dict for easier authentication
 active_clients = set() # a set for unique usernames
 heartbeats_record = {username: 0 for username in credentials}  # last heartbeat time for each user, initialised to be 0
 
-print(f"Server is listening for requests...")
-
+print(f"Server starts listening on {server_address} ...")
 while True:
     client_request, client_address = server_socket.recvfrom(1024)
     client_request = client_request.decode().split(' ')
     request_type = client_request[0]
     match request_type:
         case "AUTH":
-            active_clients = handle_AUTH()
+            handle_AUTH()
         case "HBT":
             handle_HBT()
-
+        case "LAP":
+            handle_LAP()
+        case "LPF":
+            handle_LPF()
 
